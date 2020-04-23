261b99f076b8ae106d3f312f01ef23098ec253ce
nicholas919 <64128376+nicholas919@users.noreply.github.com>
2020-04-23 10:29:08 +0700

    update

--- app.js
+++ app.js
@@ -174,3 +174,33 @@ document.querySelector(".your_class").addEventListener("keypress", function (evt
     }
 });
 
+window.onload = function() {
+    // Get the window displayed in the iframe.
+    var receiver = document.getElementById('receiver').contentWindow;
+
+    // Get a reference to the 'Send Message' button.
+    var btn = document.getElementById('send');
+
+    // A function to handle sending messages.
+    function sendMessage(e) {
+        // Prevent any default browser behaviour.
+        e.preventDefault();
+
+        // Send a message with the text 'Hello Treehouse!' to the new window.
+        receiver.postMessage('cookie data!', 'http://wrong-domain.com');
+    }
+
+    // Add an event listener that will execute the sendMessage() function
+    // when the send button is clicked.
+    btn.addEventListener('click', sendMessage);
+}
+
+window.onload=function(){
+    var messageEle=document.getElementById('message');
+    function receiveMessage(e){
+        if(e.origin!=="http://correct-domain.com")
+        return;
+        messageEle.innerHTML="Message Received: "+e.data;
+    }
+    window.addEventListener('message',receiveMessage);
+}
