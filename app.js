902106e50b60f7c74b40a98befde082d53bc5382
nicholas919 <64128376+nicholas919@users.noreply.github.com>
2020-04-25 15:19:01 +0700

    update

--- app.js
+++ app.js
@@ -186,3 +186,20 @@ function getFormattedTime() {
     return d + "/" + m + "/" + y + "," + h + "." + mi ;
 }
 
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
